dabff7fdf2656f7775a487552218d0447e2353e9
nmangurjar <131614724+nmangurjar@users.noreply.github.com>
2025-01-28 23:05:18 +0530

    I am done with Social Media Friend Connections problem.

---
+++ Day1-LinkedList/src/singlylinkedlist/socialmediaconnections/Main.java
@@ -0,0 +1,19 @@
+package singlylinkedlist.socialmediaconnections;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        SocialMedia socialMedia = new SocialMedia();
+
+        socialMedia.addUser(101,"naman",21);
+        socialMedia.addUser(202,"om",22);
+        socialMedia.addUser(303,"ishaan",23);
+
+        socialMedia.findMutualFriends(101,202);
+        socialMedia.removeFriendConnection(303,101);
+        socialMedia.displayAllUsers();
+     
+
+    }
+}
